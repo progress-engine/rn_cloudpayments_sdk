b15177bc3d16c40abd506c661d69336bd2b09885
Васин Леонид Евгеньевич <l.vasin@cft.ru>
2019-08-29 21:28:03 +0700

    оставил по одному колбэк

--- ios/RNReactNativeCloudPaymentsSdk.m
+++ ios/RNReactNativeCloudPaymentsSdk.m
@@ -6,37 +6,34 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(isValidNumber: (NSString *)cardNumber
-				  resolve: (RCTPromiseResolveBlock)resolve
-				  reject: (RCTPromiseRejectBlock)reject)
+				  block: (RCTResponseSenderBlock)block
 {
 	if([Card isCardNumberValid: cardNumber]) {
-		resolve(@YES);
+		block(@YES);
 	} else {
-		resolve(@NO);
+		block(@NO);
 	}
 };
 
 RCT_EXPORT_METHOD(getType: (NSString *)cardNumber
-				  resolve: (RCTPromiseResolveBlock)resolve
-				  reject: (RCTPromiseRejectBlock)reject)
+				  block: (RCTPromiseResolveBlock)block
 {
 	CardType cardType = [Card cardTypeFromCardNumber: cardNumber];
 	NSString *cardTypeString = [Card cardTypeToString: cardType];
 
-	resolve(cardTypeString);
+	block(cardTypeString);
 }
 
 RCT_EXPORT_METHOD(createCryptogram: (NSString *)cardNumber
 				  cardExp: (NSString *)cardExp
 				  cardCvv: (NSString *)cardCvv
 				  publicId: (NSString *)publicId
-				  resolve: (RCTPromiseResolveBlock)resolve
-				  reject: (RCTPromiseRejectBlock)reject)
+				  block: (RCTPromiseResolveBlock)block
 {
 	Card *_card = [[Card alloc] init];
 
 	NSString *cryptogram = [_card makeCardCryptogramPacket: cardNumber andExpDate:cardExp andCVV:cardCvv andMerchantPublicID:publicId];
 
-	resolve(cryptogram);
+	block(cryptogram);
 }
 @end
